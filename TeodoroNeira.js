66f1bf9b7f270f70ade731c683e5be2c6ebfec05
Teodoro Neira <teodoro.neira@sou.inteli.edu.br>
2026-05-01 18:31:38 -0300

    feat: inversão de cores quando o mouse está em cima

--- TeodoroNeira.js
+++ TeodoroNeira.js
@@ -5,43 +5,23 @@ function setup() {
 
 function draw() {
 
-    // Vermelho
-    fill(255, 0, 0); 
-    noStroke();
-    rect(98, 0, 302, 290);
+  retangulos(98, 0, 302, 290, color(255, 0, 0));      // Vermelho
+  retangulos(0, 300, 86, 100, color(0, 0, 255));      // Azul
+  retangulos(368, 357, 32, 43, color(255, 255, 0));   // Amarelo
 
-    // Azul
-    fill(0, 0, 255);
-    noStroke();
-    rect(0, 300, 86, 100);
+  retangulos(0, 288, 400, 12, color(0));         // Linha horizontal 1
+  retangulos(0, 118, 86,  25, color(0));          // Linha horizontal 2
+  retangulos(86, 0, 12, 400, color(0));          // Linha vertical 1
+  retangulos(353, 300, 15, 100, color(0));       // Linha vertical 2
+  retangulos(354, 340, 46, 19, color(0));        // Linha vertical 3
+}
 
-    // Amarelo
-    fill(255, 255, 0);
-    noStroke();
-    rect(368, 357, 32, 43);
-
-    // Linha horizontal 1
-    fill(0);
-    noStroke();
-    rect(0, 288, 400, 12);
-
-    // Linha horizontal 2
-    fill(0);
-    noStroke();
-    rect(0, 118, 86, 25);
-
-    // Linha vertical 1
-    fill(0);
-    noStroke();
-    rect(86, 0, 12, 400);
-
-    // Linha vertical 2
-    fill(0);
-    noStroke();
-    rect(353, 300, 15, 100);
-
-    // Linha vertical 3
-    fill(0);
-    noStroke();
-    rect(354, 340, 46, 19);
+function retangulos(x, y, w, h, c) {
+  if (mouseX >= x && mouseX <= x + w && mouseY >= y && mouseY <= y + h) {
+    fill(255 - (red(c) + 100), 255 - (green(c) + 100), 255 - (blue(c) + 100));
+  } else {
+    fill(c);
+  }
+  noStroke();
+  rect(x, y, w, h);
 }
